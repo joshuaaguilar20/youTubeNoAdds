241aa0df3d5f229e9171b2643b0bcfa020bf2ce7
Joshua-Aguilar <joshuaaguilar20@gmail.com>
2018-11-30 21:27:19 -0800

    add some styllleeee :)

--- src/components/App.js
+++ src/components/App.js
@@ -32,18 +32,16 @@ class App extends React.Component {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <div className="ui grid">
-          <div className="ui row">
-            <div className="eleven wide column">
-              <VideoDetail video={this.state.selectedVideo} />
-            </div>
-            <div className="five wide column">
-              <VideoList
-                onVideoSelect={this.onVideoSelect}
-                videos={this.state.videos}
-              />
-            </div>
-          </div>
+
+
+        <VideoDetail video={this.state.selectedVideo} />
+
+
+        <div className="five wide column">
+          <VideoList
+            onVideoSelect={this.onVideoSelect}
+            videos={this.state.videos}
+          />
         </div>
       </div>
     );
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -15,10 +15,10 @@ class SearchBar extends React.Component {
 
   render() {
     return (
-      <div className="search-bar ui segment" color="yellow">
+      <div className="search-bar ui segment">
         <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
-            <label>Video Search</label>
+            <label id="vidsearch">Video Search</label>
             <input
               type="text"
               value={this.state.term}
